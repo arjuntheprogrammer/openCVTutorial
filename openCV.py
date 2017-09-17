d8a171cd23e6370978afb89b3cf1d803eded35df
Arjun Gupta <saimzn4241@gmail.com>
2017-09-17 11:16:20 +0530

    plot

--- openCV.py
+++ openCV.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 img = cv2.imread('/home/arjun/Desktop/openCV/img.JPG', cv2.IMREAD_GRAYSCALE)
 # IMREAD_COLOR = 1
 # IMREAD_UNCHANGED = -1
-cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-cv2.imshow('image', img)
-cv2.resizeWindow('image', 500, 500)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+# cv2.imshow('image', img)
+# cv2.resizeWindow('image', 500, 500)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+plt.imshow(img, cmap='gray', interpolation = 'bicubic')
+plt.plot([50, 100], [80, 100], 'c', linewidth=5) #plot line
+plt.show()
+
